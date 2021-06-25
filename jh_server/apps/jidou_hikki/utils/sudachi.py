6465eb5e0634676c1fbafe3364d37df3a1c7a4f1
fariz.tumbuan <fariz.tumbuan@prosa.ai>
2021-06-25 20:30:24 +0800

    Refactor Token and Tokenizer into abstract classes

--- jh_server/apps/jidou_hikki/utils/sudachi.py
+++ jh_server/apps/jidou_hikki/utils/sudachi.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod, abstractstaticmethod
 from typing import List
 
 import jaconv
@@ -27,8 +28,68 @@ MODE_B = tokenizer.Tokenizer.SplitMode.B
 MODE_C = tokenizer.Tokenizer.SplitMode.C
 
 
-class Token(object):
-    _okurigana_template = "<ruby data-word-id={word_id}><rb>{surface}</rb><rp>(</rp><rt>{furigana}</rt><rp>)</rp>{okurigana}</ruby>"
+class Token(ABC):
+    def __repr__(self):
+        return f"<{self.__class__.__name__}: {self.word}>"
+
+    def __str__(self):
+        return self.word
+
+    @property
+    @abstractmethod
+    def word(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def word_id(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def reading_form(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def lemma(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def reading_form(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def normalized_form(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def part_of_speech(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def as_html(self):
+        raise NotImplementedError
+
+    def contains_kanji(self):
+        return any([is_kanji(ch) for ch in self.word])
+
+    def as_hiragana(self):
+        return jaconv.kata2hira(self.reading_form)
+
+    def as_katana(self):
+        return jaconv.kata2hira(self.reading_form)
+
+    def is_punctuation(self):
+        ch = self.word
+        return len(ch) == 1 and not (is_kanji(ch) or is_hiragana(ch) or is_katakana(ch))
+
+
+class SudachiToken(Token):
+    _html_template = "<ruby data-word-id={word_id}><rb>{surface}</rb><rp>(</rp><rt>{furigana}</rt><rp>)</rp>{okurigana}</ruby>"
 
     def __init__(self, morpheme: sudachipy.morpheme.Morpheme):
         self._token = morpheme
@@ -36,12 +97,6 @@ class Token(object):
         self.furigana = None
         self.okurigana = None
         self._split_okurigana()
-
-    def __repr__(self):
-        return self._token.surface()
-
-    def __str__(self):
-        return self._token.surface()
 
     def _split_okurigana(self):
         if self.contains_kanji():
@@ -65,6 +120,10 @@ class Token(object):
             self.furigana = furigana
 
     @property
+    def word(self):
+        return self._token.surface()
+
+    @property
     def word_id(self):
         return f"sudachi__{self._token.word_id()}"
 
@@ -73,16 +132,20 @@ class Token(object):
         return self._token.reading_form()
 
     @property
+    def normalized_form(self):
+        return self._token.normalized_form()
+
+    @property
     def lemma(self):
         return self._token.dictionary_form()
 
     @property
-    def meta(self):
+    def part_of_speech(self):
         return self._token.part_of_speech()
 
     def as_html(self):
         if self.contains_kanji():
-            return self._okurigana_template.format(
+            return self._html_template.format(
                 word_id=self.word_id,
                 surface=self.kanji,
                 furigana=self.furigana,
@@ -90,23 +153,38 @@ class Token(object):
             )
         return str(self)
 
-    def contains_kanji(self):
-        return any([is_kanji(ch) for ch in str(self)])
 
-    def as_hiragana(self):
-        return jaconv.kata2hira(self._token.reading_form())
+class Tokenizer(ABC):
+    @classmethod
+    @abstractmethod
+    def from_text(cls, sentence: str, *args, **kwargs) -> List[Token]:
+        """
+        Tokenize a sentence into a list of tokens.
+        """
+        raise NotImplementedError
 
-    def as_katana(self):
-        return jaconv.kata2hira(self._token.reading_form())
+    @classmethod
+    @abstractmethod
+    def normalize(cls, token: Token, *args, **kwargs) -> Token:
+        """Create a normalized token from input token (i.e. dictionary form)."""
+        raise NotImplementedError
 
-    def is_punctuation(self):
-        ch = str(self)
-        return len(ch) == 1 and not (is_kanji(ch) or is_hiragana(ch) or is_katakana(ch))
 
-
-class Tokenizer(object):
+class SudachiTokenizer(Tokenizer):
     _tokenizer = dictionary.Dictionary().create()
 
     @classmethod
-    def from_text(cls, sentence: str, split_mode: "SplitMode" = MODE_A) -> List[Token]:
-        return [Token(token) for token in cls._tokenizer.tokenize(sentence, split_mode)]
+    def from_text(
+        cls, sentence: str, split_mode: "SplitMode" = MODE_A
+    ) -> List[SudachiToken]:
+        return [
+            SudachiToken(token)
+            for token in cls._tokenizer.tokenize(sentence, split_mode)
+        ]
+
+    @classmethod
+    def normalize(cls, token: Token, split_mode: "SplitMode" = MODE_A) -> SudachiToken:
+        tokens = cls._tokenizer.tokenize(token.normalized_form, split_mode)
+        if len(tokens) == 1:
+            return SudachiToken(tokens[0])
+        raise ValueError(f"Cannot normalize {token}")
