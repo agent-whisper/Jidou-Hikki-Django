eb1a4fcb5f769162458c9cc3b4fffcb0a5652187
fariz.tumbuan <fariz.tumbuan@prosa.ai>
2021-06-29 21:22:27 +0800

    Created Sentence model (LinkedList)

--- jh_server/apps/jidou_hikki/models/notebook.py
+++ jh_server/apps/jidou_hikki/models/notebook.py
@@ -1,4 +1,7 @@
+from typing import Iterator
+
 from django.db import models, transaction
+from django.db.models import Q
 from django.contrib.auth import get_user_model
 from model_utils.models import TimeStampedModel
 
@@ -7,6 +10,40 @@ from ..tokenizer import get_tokenizer
 
 _TOKENIZER = get_tokenizer()
 _USER_MODEL = get_user_model()
+
+
+class Sentence(TimeStampedModel):
+    text = models.TextField()
+    html_template = models.TextField()
+    vocabularies = models.ManyToManyField(Vocabulary)
+    prev = models.ForeignKey(
+        "Sentence",
+        on_delete=models.SET_NULL,
+        null=True,
+        default=None,
+        related_name="next_line",
+    )
+    next = models.ForeignKey(
+        "Sentence",
+        on_delete=models.SET_NULL,
+        null=True,
+        default=None,
+        related_name="prev_line",
+    )
+
+    @transaction.atomic
+    def set_next(self, sentence: "Sentence") -> None:
+        if not self.next:
+            self.next = sentence
+            sentence.prev = self
+            self.save()
+            sentence.save()
+        else:
+            # Insert in the middle
+            self.next.prev = sentence
+            sentence.prev = self
+            sentence.next = self.next
+            self.next = sentence
 
 
 class NotePageManager(models.Manager):
@@ -22,10 +59,25 @@ class NotePage(TimeStampedModel):
 
     title = models.TextField()
     text = models.TextField()
+    sentences = models.ManyToManyField(Sentence, related_name="page")
     html = models.TextField()
     vocabularies = models.ManyToManyField(Vocabulary)
     notebook = models.ForeignKey(
         "Notebook", on_delete=models.CASCADE, related_name="pages"
+    )
+    first_line = models.ForeignKey(
+        Sentence,
+        on_delete=models.SET_NULL,
+        null=True,
+        default=None,
+        related_name="first_line_of",
+    )
+    last_line = models.ForeignKey(
+        Sentence,
+        on_delete=models.SET_NULL,
+        null=True,
+        default=None,
+        related_name="last_line_of",
     )
 
     def __str__(self):
@@ -55,6 +107,29 @@ class NotePage(TimeStampedModel):
         self.html = "<br>".join(html_lines)
         self.save()
         self.vocabularies.set(vocabularies)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=Q(first_line__isnull=True, last_line__isnull=True)
+                | Q(first_line__isnull=False, last_line__isnull=False),
+                name="cc_notepage_first_last_line_match",
+            )
+        ]
+
+    @property
+    def is_empty(self):
+        return not (self.first_line and self.last_line)
+
+    def read_contents(self) -> Iterator[Sentence]:
+        if self.is_empty:
+            return
+        sentence = self.first_line
+        yield sentence
+        while sentence.next:
+            sentence = sentence.next
+            yield sentence
+        return
 
 
 class Notebook(TimeStampedModel):
