2221fc4ea6308530d13098e8b6ccbacef5a2f86b
Fariz Tumbuan <fariz.tumbuan@prosa.ai>
2022-07-23 11:54:51 +0800

    Added django-cors-headers

--- src/settings.py
+++ src/settings.py
@@ -40,17 +40,21 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "src.apps.notebook",
     "src.apps.wordcollection",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "src.urls"
 
